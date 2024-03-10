a04d9cf73296fa03d7aafb8a6fdff0deb6a6102f
myrmik <myrmik@gmail.com>
2024-03-10 18:41:27 +0200

    boost options - first

---
+++ misc/lin_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VCPKG_ROOT="~/vcpkg"
+BuildDir="Build"
+
+# for debug cmake file
+rm -r  $BuildDir
+
+mkdir $BuildDir
+cmake -B $BuildDir "-DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
+#cmake -B $BuildDir "-DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" -DCMAKE_CXX_FLAGS=--coverage -DCMAKE_CXX_FLAGS=-pg
+
+cmake --build $BuildDir
---
+++ misc/src/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+//see  https://www.boost.org/doc/libs/1_83_0/doc/html/program_options.html
+#include <boost/program_options.hpp>
+
+
+
+
+namespace opt = boost::program_options;
+
+
+int main( int argc, char *argv[])
+{
+    opt::options_description desc("All opt");
+    desc.add_options()
+        ("apples", opt::value<int>()->default_value(0), "how many apples do you have")
+        ("oranges", opt::value<int>()->default_value(0), "how many oranges do you have")
+        ("name,n", opt::value<std::string>(), "Some name")
+        ("help,h", "produce help message")
+        ;
+
+    opt::variables_map vm;
+    opt::store(opt::parse_command_line(argc, argv, desc), vm);
+    opt::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
+
+    auto it_a = vm.find("apples");
+    auto it_o = vm.find("oranges");
+    if (it_a != vm.end() && it_o != vm.end())
+    {
+        std::cout << "Fruits count: "
+            << vm["apples"].as<int>() + vm["oranges"].as<int>()
+            << "\n";
+    }
+
+
+    if (auto it = vm.find("name"); it != vm.end())
+    {
+        std::cout << it->first << " = " << it->second.as<std::string>() << "\n";
+    }
+
+
+    return 0;
+}
