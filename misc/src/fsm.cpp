02ae208ae8014199e608c7ab737e9a03bba19c04
myrmik <myrmik@gmail.com>
2024-07-13 13:21:04 +0300

    boost::msm aka FSM,  doExercise 1  from https://theboostcpplibraries.com/boost.msm

--- misc/src/fsm.cpp
+++ misc/src/fsm.cpp
@@ -1,7 +1,17 @@
 #include <boost/msm/front/euml/euml.hpp>
 #include <boost/msm/front/euml/state_grammar.hpp>
 #include <boost/msm/back/state_machine.hpp>
+
+#include <boost/msm/back/tools.hpp>
+#include <boost/msm/back/metafunctions.hpp>
+#include <boost/mpl/for_each.hpp>
+
+
 #include <iostream>
+
+
+
+
 
 //------------------------------------------------------------------
 //Examples from https://theboostcpplibraries.com/boost.msm
@@ -290,8 +300,98 @@ namespace NExample_6
 
 
 //------------------------------------------------------------------
-namespace N_MY
+namespace NExercises
 {
+	/*
+		1. Create a state machine for a window that can be closed, opened or tilted. 
+		   A closed window can be opened or tilted. An open window can’t be tilted though without closing it first. 
+		   Nor can a tilted window be opened without closing it first. 
+		   Test your state machine by opening and tilting your window a couple of times. 
+		   Use current_state() to write states to standard output.
+	*/
+
+	namespace msm = boost::msm;
+	using namespace boost::msm::front::euml;
+
+	//windows state - ws
+	BOOST_MSM_EUML_STATE((), ws_closed)
+	BOOST_MSM_EUML_STATE((), ws_opened)
+	BOOST_MSM_EUML_STATE((), ws_tilted)
+
+	//windows action - wa
+	BOOST_MSM_EUML_EVENT(wa_close)
+	BOOST_MSM_EUML_EVENT(wa_open)
+	BOOST_MSM_EUML_EVENT(wa_tilt)
+
+	BOOST_MSM_EUML_TRANSITION_TABLE((
+		ws_closed + wa_open == ws_opened,
+		ws_opened + wa_close == ws_closed,
+		ws_closed + wa_tilt == ws_tilted,
+		ws_tilted + wa_close == ws_closed
+		), window_transition_table)
+
+	BOOST_MSM_EUML_DECLARE_STATE_MACHINE(
+		(window_transition_table, init_ << ws_closed),
+		window_state_machine)
+
+
+	std::string getStateName(int stateId) 
+	{
+		switch (stateId)
+		{
+		case 0: return "closed";
+		case 1: return "opened";
+		case 2: return "tilted";
+
+		}
+		return "__UNCNOWN__";
+	}
+
+	template<class FSM>
+	std::string getCurStateName(const FSM& fsm)
+	{
+		return getStateName(*fsm.current_state());
+	}
+
+#define DO_CASE(action) \
+do \
+{ \
+	fsmWindow.process_event(action); \
+	std::cout << "The action is <"#action">, state change to --> " << getCurStateName(fsmWindow) << "\n"; \
+} \
+while (false)
+//[END] DO_CASE
+
+
+	void main()
+	{
+		msm::back::state_machine<window_state_machine> fsmWindow;
+
+		std::cout << "START STATE: " << getCurStateName(fsmWindow) << '\n';
+		std::cout << "--- NORMAL ---\n";
+
+		DO_CASE(wa_close);
+		DO_CASE(wa_open);
+		DO_CASE(wa_close);
+		DO_CASE(wa_tilt);
+		DO_CASE(wa_close);
+
+
+		std::cout << "--- TRY tilt opened  ---\n";
+		DO_CASE(wa_close);
+		DO_CASE(wa_open);
+		DO_CASE(wa_tilt);
+		DO_CASE(wa_close);
+
+		std::cout << "--- TRY open tilted  ---\n";
+		DO_CASE(wa_close);
+		DO_CASE(wa_tilt);
+		DO_CASE(wa_open);
+		DO_CASE(wa_close);
+	}
+
+
+#undef DO_CASE
 }
 
 
@@ -310,5 +410,9 @@ void fsm_main()
 	NExample_5::main();
 	std::cout << "\n---------------------------------------------------------------\n" << "#6\n";
 	NExample_6::main();
+
+	std::cout << "\n---------------------------------------------------------------\n" << "#6\n";
+	NExercises::main();
+
 }
 
